3b904098ab3d0786e0286c96ddf9097eb1ff33a1
HariRaagavanathar C R <hari.raghavandhar@myntra.com>
2021-07-31 16:57:36 +0530

    added build folder

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,3 +9,11 @@ app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`));
 app.get('/api', (req, res)=>{
     res.json({ message: "Hello World"})
 })
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
